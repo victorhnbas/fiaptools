4d1cf520684b4929e0da3c4082bc59e1bc954d73
Victor Henrique <123006884+victorhnbas@users.noreply.github.com>
2023-08-31 00:34:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 $("img").attr("draggable","false")
+
+setInterval(() => {
+    var date = new Date()
+    var clock = document.getElementById("clock")
+    clock.innerHTML = 
+    date.getHours()+":"+
+    date.getMinutes()+":"+
+    date.getSeconds()
+}, 1000)
